c1030121579be1c6e30e9c7387b87de3f9693e66
deshs-otg <deshraj@techteam.onetouchgrade.com>
2025-04-30 19:13:27 +0530

    new migration updates

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,68 +9,22 @@ class Listing extends Model
     use HasFactory;
 
     protected $fillable = [
-        'reference_no',
-        'title',
-        'title_deed',
-        'property_type',
-        'offering_type',
-        'size',
-        'unit_no',
-        'bedrooms',
-        'bathrooms',
-        'parking',
-        'furnished',
-        'total_plot_size',
-        'plot_size',
-        'built_up_area',
-        'layout_type',
-        'project_name',
-        'project_status',
-        'sale_type',
-        'developer_id',
-        'build_year',
-        'customer',
-        'rera_permit',
-        'rera_number',
-        'rera_permit_issue_date',
-        'rera_expiration_date',
-        'contract_expiry_date',
-        'rental_period',
-        'rprice_price',
-        'payment_method',
-        'financial_status',
-        'sale_type_1',
-        'title(english)',
-        'title(arabic)',
-        'description(english)',
-        'description(arabic)',
-        'geopoints',
-        'listing_owner',
-        'landlord_name',
-        'landlord_contact',
-        'location_id',
-        'availability',
-        'available_from',
-        'emirate_amount',
-        'payment_option',
-        'no_of_cheques',
-        'contract_charges',
-        'financial_status_id',
-        'contract_expiry',
-        'floor_plan',
-        'qr_code',
-        'brochure',
-        'video_url',
-        '360_view_url',
-        'photos_urls',
-        'status',
-        'property_finder',
-        'dubizzle',
-        'website',
-        'watermark',
-       'company_id',
-       'agent_id'
+        'reference_no', 'title', 'property_type', 'offering_type', 'size',
+        'unit_no', 'bedrooms', 'bathrooms', 'parking', 'furnished',
+        'total_plot_size', 'plot_size', 'built_up_area', 'layout_type',
+        'project_name', 'project_status', 'sale_type', 'developer_id', 'build_year',
+        'customer', 'rera_permit_number', 'rera_issue_date', 'rera_expiration_date',
+        'contract_expiry_date', 'rental_period', 'price', 'payment_method',
+        'financial_status', 'sale_type_1', 'title_en', 'title_ar', 'desc_en',
+        'desc_ar', 'geopoints', 'listing_owner', 'landlord_name', 'landlord_contact',
+        'pf_location', 'bayut_location', 'availability', 'available_from',
+        'emirate_amount', 'payment_option', 'no_of_cheques', 'contract_charges',
+        'financial_status_id', 'contract_expiry', 'floor_plan', 'qr_code', 'brochure',
+        'video_url', '360_view_url', 'watermark', 'pf_enable', 'bayut_enable',
+        'dubizzle_enable', 'website_enable', 'company_id', 'agent_id', 'owner_id',
+        'status'
     ];
+    
     
 
     public function photos()
@@ -98,10 +52,23 @@ class Listing extends Model
         return $this->belongsTo(User::class, 'developer_id');
     }
 
-    public function location()
+        public function location()
     {
-        return $this->belongsTo(Location::class, 'location_id');
+        return $this->belongsTo(Location::class, 'pf_location');
     }
+
+    
+
+        public function pfLocation()
+    {
+        return $this->belongsTo(Location::class, 'pf_location');
+    }
+
+    public function bayutLocation()
+    {
+        return $this->belongsTo(Location::class, 'bayut_location');
+    }
+
 
     public function company()
     {
